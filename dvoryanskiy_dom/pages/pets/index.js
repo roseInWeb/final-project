8794c45edae8887c744b80eb74ba7ec3f0d8dd1c
RoseInWeb <e.a.s.catinweb13@gmail.com>
2025-03-19 20:35:50 +0400

    19.03.25

--- dvoryanskiy_dom/pages/pets/index.js
+++ dvoryanskiy_dom/pages/pets/index.js
@@ -88,12 +88,11 @@ const pets = [
       "parasites": ["lice", "fleas"]
     }
 ];
-let card = document.querySelectorAll('.card');
+// let card = document.querySelectorAll('.card');
 const cardsCont = document.querySelector('.cards-cont');
 const allPagesPetsFunc = () => {
     const arr = [];
     for (let i = 0; i < 6; i++) {
-        pets.sort(() => Math.random() - 0.5);
         for (let j = 0; j < pets.length; j++) {
             arr.push(pets[j]);
         }
@@ -116,139 +115,160 @@ function main(num, num2) {
         }
     };
     lala();
-    card = document.querySelectorAll('.card');
-    card.forEach((el) => el.addEventListener('click', openModal));
+    // card = document.querySelectorAll('.card');
+    // card.forEach((el) => el.addEventListener('click', openModal));
 }
 
 main(0, 7);
 
-const nextBtn = document.querySelector('.next-ar');
-const prevBtn = document.querySelector('.prev-ar');
-const nextAllBtn = document.querySelector('.next-all-ar');
-const prevAllBtn = document.querySelector('.prev-all-ar');
-const navigNum = document.querySelector('.navig-num');
+// const nextBtn = document.querySelector('.next-ar');
+// const prevBtn = document.querySelector('.prev-ar');
+// const nextAllBtn = document.querySelector('.next-all-ar');
+// const prevAllBtn = document.querySelector('.prev-all-ar');
+// const navigNum = document.querySelector('.navig-num');
 
-nextBtn.addEventListener('click', () => {
-    if (+navigNum.textContent !== 5) {
-		main(+navigNum.textContent * 8, +navigNum.textContent * 8 + 7);
-		navigNum.innerHTML = `${+navigNum.textContent + 1}`;
-		prevBtn.removeAttribute('disabled');
-		prevAllBtn.removeAttribute('disabled');
-	} else {
-		main(+navigNum.textContent * 8, +navigNum.textContent * 8 + 7);
-		navigNum.innerHTML = `${+navigNum.textContent + 1}`;
-		nextBtn.setAttribute('disabled', '');
-		nextAllBtn.setAttribute('disabled', '');
-	}
-    card = document.querySelectorAll('.card');
-});
+// nextBtn.addEventListener('click', () => {
+//     if (+navigNum.textContent !== 5) {
+// 		main(+navigNum.textContent * 8, +navigNum.textContent * 8 + 7);
+// 		navigNum.innerHTML = `${+navigNum.textContent + 1}`;
+// 		prevBtn.removeAttribute('disabled');
+// 		prevAllBtn.removeAttribute('disabled');
+// 	} else {
+// 		main(+navigNum.textContent * 8, +navigNum.textContent * 8 + 7);
+// 		navigNum.innerHTML = `${+navigNum.textContent + 1}`;
+// 		nextBtn.setAttribute('disabled', '');
+// 		nextAllBtn.setAttribute('disabled', '');
+// 	}
+//     card = document.querySelectorAll('.card');
+// });
 
-prevBtn.addEventListener('click', () => {
-	if (+navigNum.textContent !== 2) {
-		main(+navigNum.textContent * 8 - 16, +navigNum.textContent * 8 - 9);
-		navigNum.innerHTML = `${+navigNum.textContent - 1}`;
-		nextBtn.removeAttribute('disabled');
-		nextAllBtn.removeAttribute('disabled');
-	} else {
-		main(+navigNum.textContent * 8 - 16, +navigNum.textContent * 8 - 9);
-		navigNum.innerHTML = `${+navigNum.textContent - 1}`;
-		prevBtn.setAttribute('disabled', '');
-		prevAllBtn.setAttribute('disabled', '');
-	}
-    card = document.querySelectorAll('.card');
-});
+// prevBtn.addEventListener('click', () => {
+// 	if (+navigNum.textContent !== 2) {
+// 		main(+navigNum.textContent * 8 - 16, +navigNum.textContent * 8 - 9);
+// 		navigNum.innerHTML = `${+navigNum.textContent - 1}`;
+// 		nextBtn.removeAttribute('disabled');
+// 		nextAllBtn.removeAttribute('disabled');
+// 	} else {
+// 		main(+navigNum.textContent * 8 - 16, +navigNum.textContent * 8 - 9);
+// 		navigNum.innerHTML = `${+navigNum.textContent - 1}`;
+// 		prevBtn.setAttribute('disabled', '');
+// 		prevAllBtn.setAttribute('disabled', '');
+// 	}
+//     card = document.querySelectorAll('.card');
+// });
 
-nextAllBtn.addEventListener('click', () => {
-	main(40, 47);
-	navigNum.innerHTML = '6';
-	nextBtn.setAttribute('disabled', '');
-	nextAllBtn.setAttribute('disabled', '');
-	prevBtn.removeAttribute('disabled');
-	prevAllBtn.removeAttribute('disabled');
-});
+// nextAllBtn.addEventListener('click', () => {
+// 	main(40, 47);
+// 	navigNum.innerHTML = '6';
+// 	nextBtn.setAttribute('disabled', '');
+// 	nextAllBtn.setAttribute('disabled', '');
+// 	prevBtn.removeAttribute('disabled');
+// 	prevAllBtn.removeAttribute('disabled');
+// });
 
-prevAllBtn.addEventListener('click', () => {
-	main(0, 7);
-	navigNum.innerHTML = '1';
-	prevBtn.setAttribute('disabled', '');
-	prevAllBtn.setAttribute('disabled', '');
-	nextBtn.removeAttribute('disabled');
-	nextAllBtn.removeAttribute('disabled');
-});
+// prevAllBtn.addEventListener('click', () => {
+// 	main(0, 7);
+// 	navigNum.innerHTML = '1';
+// 	prevBtn.setAttribute('disabled', '');
+// 	prevAllBtn.setAttribute('disabled', '');
+// 	nextBtn.removeAttribute('disabled');
+// 	nextAllBtn.removeAttribute('disabled');
+// });
 
+
+// const adMemuBtn = document.querySelector('.menu');
+// const overflow = document.querySelector('.overflow');
+// const html = document.querySelector('html');
+// const menu = document.querySelector('ul');
+// const menuLi = document.querySelectorAll('li');
+// const header = document.querySelector('.header-container');
+// let num = 1;
+
+// function adMenuOpen() {
+//     if (num % 2) {
+//         menu.style.right = '0';
+//         overflow.style.right = '0';
+//         header.style.position = 'fixed';
+//         html.classList.add('html');
+//         num++;
+//         adMemuBtn.style.transform = 'rotate(90deg)';
+//     } else {
+//         menu.style.right = '-320px';
+//         overflow.style.right = '-100%';
+//         html.classList.remove('html');
+//         adMemuBtn.style.transform = 'rotate(0deg)';
+//         num--;
+//     }
+// }
+
+// function adMenuClose() {
+//     menu.style.right = '-320px';
+//     overflow.style.right = '-100%';
+//     html.classList.remove('html');
+//     adMemuBtn.style.transform = 'rotate(0deg)';
+//     num++;
+// }
+// adMemuBtn.addEventListener('click', adMenuOpen);
+// menuLi.forEach((el) => el.addEventListener('click', adMenuClose));
+// overflow.addEventListener('click', adMenuClose);
 
 const adMemuBtn = document.querySelector('.menu');
-const overflow = document.querySelector('.overflow');
 const html = document.querySelector('html');
 const menu = document.querySelector('ul');
 const menuLi = document.querySelectorAll('li');
 const header = document.querySelector('.header-container');
-let num = 0;
+let num = 1;
 
-function adMenuOpen() {
-    if (num % 2 == 0) {
+function adMenu() {
+    if (num % 2) {
         menu.style.right = '0';
-        overflow.style.right = '0';
-        header.style.position = 'fixed';
-        html.classList.add('html');
-        num++;
         adMemuBtn.style.transform = 'rotate(90deg)';
+        num++;
     } else {
         menu.style.right = '-320px';
-        overflow.style.right = '-100%';
-        html.classList.remove('html');
         adMemuBtn.style.transform = 'rotate(0deg)';
         num++;
     }
 }
-function adMenuClose() {
-    menu.style.right = '-320px';
-    overflow.style.right = '-100%';
-    html.classList.remove('html');
-    adMemuBtn.style.transform = 'rotate(0deg)';
-    num++;
-}
-adMemuBtn.addEventListener('click', adMenuOpen);
-menuLi.forEach((el) => el.addEventListener('click', adMenuClose));
-overflow.addEventListener('click', adMenuClose);
-
+adMemuBtn.addEventListener('click', adMenu);
+menuLi.forEach((el) => el.addEventListener('click', adMenu));
 
 // modalWindow
-const modalWindow = document.querySelector('.modal');
-const modalBtn = document.querySelector('.modal-button');
-const petName = document.querySelector('.pet-name');
-const petImage = document.querySelector('.image');
-const petAge = document.querySelector('.span-li-age');
-const petInocul = document.querySelector('.span-li-ino');
-const petDis = document.querySelector('.span-li-dis');
-const petParas = document.querySelector('.span-li-par');
-const petBreed = document.querySelector('.breed');
-const petDescription = document.querySelector('.description');
+// const modalWindow = document.querySelector('.modal');
+// const modalBtn = document.querySelector('.modal-button');
+// const petName = document.querySelector('.pet-name');
+// const petImage = document.querySelector('.image');
+// const petAge = document.querySelector('.span-li-age');
+// const petInocul = document.querySelector('.span-li-ino');
+// const petDis = document.querySelector('.span-li-dis');
+// const petParas = document.querySelector('.span-li-par');
+// const petBreed = document.querySelector('.breed');
+// const petDescription = document.querySelector('.description');
 
-function openModal() {
-    const thisCard = this.children[1].textContent;
-    for (let i = 0; i < pets.length; i++) {
-        if (thisCard === pets[i].name) {
-            modalWindow.style.display = 'block';
-            petName.innerHTML = `${pets[i].name}`;
-            petImage.src = `${pets[i].img}`;
-            petBreed.innerHTML = `${pets[i].breed}`;
-            petDescription.innerHTML = `${pets[i].description}`;
-            petAge.innerHTML = `${pets[i].age}`;
-            petParas.innerHTML = `${pets[i].parasites}`;
-            petDis.innerHTML = `${pets[i].diseases}`;
-            petInocul.innerHTML = `${pets[i].inoculations}`;
-        }
-    }
-    overflow.style.right = '0';
-    html.classList.add('html');
-}
-card.forEach((el) => el.addEventListener('click', openModal));
+// function openModal() {
+//     const thisCard = this.children[1].textContent;
+//     for (let i = 0; i < pets.length; i++) {
+//         if (thisCard === pets[i].name) {
+//             modalWindow.style.display = 'block';
+//             petName.innerHTML = `${pets[i].name}`;
+//             petImage.src = `${pets[i].img}`;
+//             petBreed.innerHTML = `${pets[i].breed}`;
+//             petDescription.innerHTML = `${pets[i].description}`;
+//             petAge.innerHTML = `${pets[i].age}`;
+//             petParas.innerHTML = `${pets[i].parasites}`;
+//             petDis.innerHTML = `${pets[i].diseases}`;
+//             petInocul.innerHTML = `${pets[i].inoculations}`;
+//         }
+//     }
+//     overflow.style.right = '0';
+//     html.classList.add('html');
+// }
+// card.forEach((el) => el.addEventListener('click', openModal));
 
-function closeModal() {
-    modalWindow.style.display = 'none';
-    overflow.style.right = '-100%';
-    html.classList.remove('html');
-}
-overflow.addEventListener('click', closeModal);
-modalBtn.addEventListener('click', closeModal);
+// function closeModal() {
+//     modalWindow.style.display = 'none';
+//     overflow.style.right = '-100%';
+//     html.classList.remove('html');
+// }
+// overflow.addEventListener('click', closeModal);
+// modalBtn.addEventListener('click', closeModal);
